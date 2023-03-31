c7e9f54cff336e67bbc666b4539e1ef817ff59ee
moeunchhorvorn03 <moeunchhorvorn03@gmail.com>
2023-03-31 10:37:48 +0700

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <p ref="p">{{ lastName }} {{ firstName }}</p>
+    <button @click="handleClick">green</button>
+    <button @click="handle">blue</button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  name: 'Home',
+  components: {  },
+  setup(){
+    let firstName = 'Vorn'
+    let lastName = 'Chhor'
+
+    const p = ref(null)
+    
+    const handleClick = () => {
+      console.log("You've clicked")
+      p.value.classList.add('active')
+    }
+    const handle = () => {
+      p.value.classList.remove('active')
+    }
+
+    return{
+      firstName, 
+      lastName,
+      handleClick,
+      handle,
+      p
+    }
+  }
+}
+</script>
+
+<style scoped>
+.active {
+  background: green;
+}
+</style>
